798ceea6accba6b129b47240b2c5b2684cd57820
imperialvictor <imperialvictor15@gmail.com>
2022-03-22 18:51:51 +0100

    Final styling

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -7,7 +7,7 @@ interface Props {
 const StartButton: React.FC<Props> = (props) => {
   return (
     <button className="start-btn" onClick={props.onClick}>
-      Play
+      PLAY
     </button>
   );
 };
